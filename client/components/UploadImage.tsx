5eb5dd4ba6cadba614dd5e29d69f81c7c8ad67b1
Parv Sharma <sharmaparv.2004@gmail.com>
2024-09-08 12:29:02 +0530

    Adds popup when uploading without connecting wallet

--- client/components/UploadImage.tsx
+++ client/components/UploadImage.tsx
@@ -1,58 +1,136 @@
-"use client"
+"use client";
 import { BACKEND_URL, CLOUDFRONT_URL } from "@/utils";
 import axios from "axios";
 import { useState } from "react";
 
-export function UploadImage({ onImageAdded, image }: {
-    onImageAdded: (image: string) => void;
-    image?: string;
+export function UploadImage({
+  onImageAdded,
+  image,
+}: {
+  onImageAdded: (image: string) => void;
+  image?: string;
 }) {
-    const [uploading, setUploading] = useState(false);
+  const [uploading, setUploading] = useState(false);
+  const [signinError, setSigninError] = useState(false);
 
-    async function onFileSelect(e: any) {
-        setUploading(true);
-        try {
-            const file = e.target.files[0];
-            const response = await axios.get(`${BACKEND_URL}/v1/user/presignedUrl`, {
-                headers: {
-                    "Authorization": localStorage.getItem("token")
-                }
-            });
-            const presignedUrl = response.data.preSignedUrl;
-            const formData = new FormData();
-            formData.set("bucket", response.data.fields["bucket"])
-            formData.set("X-Amz-Algorithm", response.data.fields["X-Amz-Algorithm"]);
-            formData.set("X-Amz-Credential", response.data.fields["X-Amz-Credential"]);
-            formData.set("X-Amz-Algorithm", response.data.fields["X-Amz-Algorithm"]);
-            formData.set("X-Amz-Date", response.data.fields["X-Amz-Date"]);
-            formData.set("key", response.data.fields["key"]);
-            formData.set("Policy", response.data.fields["Policy"]);
-            formData.set("X-Amz-Signature", response.data.fields["X-Amz-Signature"]);
-            formData.set("X-Amz-Algorithm", response.data.fields["X-Amz-Algorithm"]);
-            formData.append("file", file);
-            const awsResponse = await axios.post(presignedUrl, formData);
+  async function onFileSelect(e: any) {
+    setUploading(true);
+    try {
+      const file = e.target.files[0];
+      const response = await axios.get(`${BACKEND_URL}/v1/user/presignedUrl`, {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+      });
+      const presignedUrl = response.data.preSignedUrl;
+      const formData = new FormData();
+      formData.set("bucket", response.data.fields["bucket"]);
+      formData.set("X-Amz-Algorithm", response.data.fields["X-Amz-Algorithm"]);
+      formData.set(
+        "X-Amz-Credential",
+        response.data.fields["X-Amz-Credential"]
+      );
+      formData.set("X-Amz-Algorithm", response.data.fields["X-Amz-Algorithm"]);
+      formData.set("X-Amz-Date", response.data.fields["X-Amz-Date"]);
+      formData.set("key", response.data.fields["key"]);
+      formData.set("Policy", response.data.fields["Policy"]);
+      formData.set("X-Amz-Signature", response.data.fields["X-Amz-Signature"]);
+      formData.set("X-Amz-Algorithm", response.data.fields["X-Amz-Algorithm"]);
+      formData.append("file", file);
+      const awsResponse = await axios.post(presignedUrl, formData);
 
-            onImageAdded(`${CLOUDFRONT_URL}/${response.data.fields["key"]}`);
-        } catch(e) {
-            console.log(e)
-        }
-        setUploading(false);
+      onImageAdded(`${CLOUDFRONT_URL}/${response.data.fields["key"]}`);
+    } catch (e) {
+    //   console.log(e);
+      setSigninError(true);
     }
+    setUploading(false);
+  }
 
-    if (image) {
-        return <img className={"p-2 w-96 rounded"} src={image} />
-    }
-
-    return <div>
-        <div className="w-40 h-40 rounded border text-2xl cursor-pointer">
-                <div className="h-full flex justify-center flex-col relative w-full">
-                    <div className="h-full flex justify-center w-full pt-16 text-4xl">
-                    {uploading ? <div className="text-sm">Loading...</div> : <>
-                        +
-                        <input className="w-full h-full bg-red-400 w-40" type="file" style={{position: "absolute", opacity: 0, top: 0, left: 0, bottom: 0, right: 0, width: "100%", height: "100%"}} onChange={onFileSelect} />
-                    </>}
-                </div>
-            </div>
+  if (signinError) {
+    return (
+      <div
+        id="toast-warning"
+        className="flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+        role="alert"
+      >
+        <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-orange-500 bg-orange-100 rounded-lg dark:bg-orange-700 dark:text-orange-200">
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z" />
+          </svg>
+          <span className="sr-only">Warning icon</span>
         </div>
+        <div className="ms-3 text-sm font-normal">
+          Connect Your Wallet.
+        </div>
+        <button
+          type="button"
+          className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+          data-dismiss-target="#toast-warning"
+          aria-label="Close"
+          onClick={()=>setSigninError(false)}
+        >
+          <span className="sr-only">Close</span>
+          <svg
+            className="w-3 h-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 14"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+            />
+          </svg>
+        </button>
+      </div>
+    );
+  }
+
+  if (image) {
+    return <img className={"p-2 w-96 rounded"} src={image} />;
+  }
+
+  return (
+    <div>
+      <div className="w-40 h-40 rounded border text-2xl cursor-pointer">
+        <div className="h-full flex justify-center flex-col relative w-full">
+          <div className="h-full flex justify-center w-full pt-16 text-4xl">
+            {uploading ? (
+              <div className="text-sm">Loading...</div>
+            ) : (
+              <>
+                +
+                <input
+                  className="w-full h-full bg-red-400 w-40"
+                  type="file"
+                  style={{
+                    position: "absolute",
+                    opacity: 0,
+                    top: 0,
+                    left: 0,
+                    bottom: 0,
+                    right: 0,
+                    width: "100%",
+                    height: "100%",
+                  }}
+                  onChange={onFileSelect}
+                />
+              </>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
+  );
 }
