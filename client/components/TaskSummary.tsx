7b553ced9540123530543e5ad0cd28bfb02cc6e7
Arshdeep-13 <arshdeeprooprai@gmail.com>
2024-09-09 23:44:16 +0530

    client analytics ui changes

---
+++ client/components/TaskSummary.tsx
@@ -0,0 +1,120 @@
+import CardDataStats from "./CardDataStats";
+
+const TaskSummary = ({
+  amtSpent,
+  totalTasks,
+  doneTasks,
+  pendingTasks,
+}: {
+  amtSpent: number;
+  totalTasks: number;
+  doneTasks: number;
+  pendingTasks: number;
+}) => {
+  return (
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5 p-6">
+      <CardDataStats
+        title="Amount Spent"
+        total={`$ ${amtSpent}`}
+        rate="4.35%"
+        levelUp
+      >
+        <div className="flex justify-start w-full items-start">
+          <div className="bg-gray-300 p-2 rounded-full">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z"
+              />
+            </svg>
+          </div>
+        </div>
+      </CardDataStats>
+      <CardDataStats
+        title="Total Tasks"
+        total={`${totalTasks}`}
+        rate="2.59%"
+        levelUp
+      >
+        <div className="flex justify-start w-full items-start">
+          <div className="bg-gray-300 p-2 rounded-full">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M8.242 5.992h12m-12 6.003H20.24m-12 5.999h12M4.117 7.495v-3.75H2.99m1.125 3.75H2.99m1.125 0H5.24m-1.92 2.577a1.125 1.125 0 1 1 1.591 1.59l-1.83 1.83h2.16M2.99 15.745h1.125a1.125 1.125 0 0 1 0 2.25H3.74m0-.002h.375a1.125 1.125 0 0 1 0 2.25H2.99"
+              />
+            </svg>
+          </div>
+        </div>
+      </CardDataStats>
+      <CardDataStats
+        title="Done Tasks"
+        total={`${doneTasks}`}
+        rate="0.95%"
+        levelDown
+      >
+        <div className="flex justify-start w-full items-start">
+          <div className="bg-gray-300 p-2 rounded-full">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+          </div>
+        </div>
+      </CardDataStats>
+      <CardDataStats
+        title="Pending Tasks"
+        total={`${pendingTasks}`}
+        rate="0.43%"
+        levelUp
+      >
+        <div className="flex justify-start w-full items-start">
+          <div className="bg-gray-300 p-2 rounded-full">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+          </div>
+        </div>
+      </CardDataStats>
+    </div>
+  );
+};
+
+export default TaskSummary;
