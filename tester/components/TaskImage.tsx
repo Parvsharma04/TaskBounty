1e367b26457ace2b3c25f7e87cf39463d29067ae
Parv Sharma <sharmaparv.2004@gmail.com>
2024-09-14 18:22:57 +0530

    Bug

--- tester/components/TaskImage.tsx
+++ tester/components/TaskImage.tsx
@@ -6,7 +6,7 @@ export function TaskImage({
   onSelect: () => void;
 }) {
   return (
-    <div className="relative w-[200px] h-[250px] rounded-[20px] bg-gradient-to-b from-[rgba(58,56,56,0.623)] to-[rgb(31,31,31)] shadow-[0_25px_50px_rgba(0,0,0,0.55)] cursor-pointer transition-transform duration-300 hover:scale-90">
+    <div className="relative w-[200px] h-[250px] rounded-[20px] bg-gray-800 shadow-[0_25px_50px_rgba(0,0,0,0.55)] cursor-pointer transition-transform duration-300 hover:scale-90">
       <img
         onClick={onSelect}
         className="p-8 rounded-t-lg w-full h-full object-cover"
