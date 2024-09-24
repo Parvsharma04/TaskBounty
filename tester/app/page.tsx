44fcabf549673b818ad8788054b73b6affc78a1b
Parv Sharma <sharmaparv.2004@gmail.com>
2024-09-24 17:44:57 +0530

    Adds locomotive scroll

--- tester/app/page.tsx
+++ tester/app/page.tsx
@@ -1,9 +1,18 @@
+"use client"
 import Featured from "@/components/Featured";
 import Footer from "@/components/Footer";
 import FAQ from "../components/FAQ/index";
 import HeroSection from "../components/HeroSection/index";
-
+import { useEffect } from "react";
 export default function LandingPage() {
+
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll();
+    })();
+  }, []); 
+
   return (
     <main className="bg-black">
       <HeroSection />
