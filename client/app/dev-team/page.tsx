82791ca224c030f71e54957ce6fa4cd6fc4a20be
Parv Sharma <sharmaparv.2004@gmail.com>
2024-09-29 19:22:06 +0530

    Bug

--- client/app/dev-team/page.tsx
+++ client/app/dev-team/page.tsx
@@ -1,23 +1,8 @@
-<<<<<<< HEAD
 import TeamPage from '../../components/DevTeam/Team';
-=======
-import TeamPage from "../../components/DevTeam/Team";
->>>>>>> 5428654f372888fd6f6caf07fac11d7ee6aa8ff3
 
 const DevTeam = () => {
   return (
     <>
-<<<<<<< HEAD
-    <div className='h-full bg-black'>
-
-    <TeamPage/>
-    </div>
-    </>
-  )
-}
-
-export default DevTeam
-=======
       <div className="h-full bg-black">
         <TeamPage />
       </div>
@@ -26,4 +11,3 @@ export default DevTeam
 };
 
 export default DevTeam;
->>>>>>> 5428654f372888fd6f6caf07fac11d7ee6aa8ff3
