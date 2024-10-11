5b7272bd408726a77cc4a0945ca6c48c5ebdd80a
Parv Sharma <sharmaparv.2004@gmail.com>
2024-10-11 17:56:51 +0530

    Adds Error Page

---
+++ parent/app/not-found.tsx
@@ -0,0 +1,11 @@
+import ErrorPage from "@/components/Error";
+
+const Error = () => {
+  return (
+    <div className="flex justify-center items-center w-full h-screen bg-black">
+      <ErrorPage />
+    </div>
+  );
+};
+
+export default Error;
