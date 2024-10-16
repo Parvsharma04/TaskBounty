8ec09245d5c674b398fe69f8b948064e215a0619
Arshdeep-13 <arshdeeprooprai@gmail.com>
2024-10-16 15:01:37 +0530

    style component changes

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["i.imgur.com", "d2evdzd5kkyi1f.cloudfront.net"],
   },
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 export default nextConfig;
