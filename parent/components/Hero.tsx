f83d9b5331536644239e5396204a84fc341396f9
Arshdeep-13 <arshdeeprooprai@gmail.com>
2024-10-16 14:16:38 +0530

    parent landing button change

--- parent/components/Hero.tsx
+++ parent/components/Hero.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { motion } from "framer-motion";
 import { WavyBackground } from "./ui/wavy-background";
+import Link from "next/link";
 
 export function Hero() {
   return (
@@ -29,7 +30,10 @@ export function Hero() {
       >
         <div className="flex items-center justify-center">
           <div className="relative group">
-            <button className="relative inline-block p-px font-semibold leading-6 text-white bg-gray-800 shadow-2xl cursor-pointer rounded-xl shadow-zinc-900 transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95 border-white">
+            <Link
+              href="/services"
+              className="relative inline-block p-px font-semibold leading-6 text-white bg-gray-800 shadow-2xl cursor-pointer rounded-xl shadow-zinc-900 transition-transform duration-300 ease-in-out hover:scale-105 active:scale-95 border-white"
+            >
               <span className="absolute inset-0 rounded-xl bg-gradient-to-r from-teal-400 via-blue-500 to-purple-500 p-[2px] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
 
               <span className="relative z-10 block px-6 py-3 rounded-xl bg-gray-950">
@@ -53,7 +57,7 @@ export function Hero() {
                   </svg>
                 </div>
               </span>
-            </button>
+            </Link>
           </div>
         </div>
       </motion.div>
